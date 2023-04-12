2e4413f02e8d8e0715964d2e5f813bb55ccb9145
lnyrlyed <yang.ruini@northeastern.edu>
2023-04-12 16:58:30 -0400

    register and login

--- app.js
+++ app.js
@@ -1,21 +1,59 @@
-import express from 'express';
-import cors from 'cors'
-import mongoose from "mongoose";
-import { config } from 'dotenv'
-config()
-
-const database = process.env.DB_CONNECTION_STRING;
-const CONNECTION_STRING = `${database}`
-mongoose.connect(CONNECTION_STRING);
-// mongoose.connect('mongodb+srv://CS5610Team21:CS5610Team21@travelplanner.9rhwihg.mongodb.net/?retryWrites=true&w=majority');
-
+import express from 'express'
 import HelloController from "./controllers/test/HelloCotroller.js";
-import TestController from "./controllers/test/test-controller.js";
+import UsersController from "./controllers/users/users-controller.js"
+import AuthenticationController from "./controllers/users/auth-controller.js";
+
+import cors from 'cors'
+import session from 'express-session'
+import dotenv from 'dotenv'
+import mongoose from "mongoose";
+
+
+dotenv.config();
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
+mongoose.connect(CONNECTION_STRING).then(() => console.log('DB started'))
+    .catch(() => () => console.log(error.message));
 
 const app = express()
-app.use(cors())
-app.use(express.json());
 
-HelloController(app)
-TestController(app)
+// cross network region
+app.use(cors(
+    {
+        // support cookie header
+        credentials: true,
+        // must whitelists allowed domains(if using credentials)
+        // http://localhost:3000
+        origin: ['http://localhost:3000', process.env.CORS_ORIGIN]
+    }));
+
+// Secret is used in hashing the session ID
+const SECRET = 'process.env.SECRET';
+//session configure
+let sess = {
+    secret: SECRET,
+    // forces a new session to be saved.
+    saveUninitialized: true,
+    // enforces that the session is resaved against the server store on each request
+    resave: true,
+    cookie: {
+        secure: process.env.NODE_ENV === "production",
+        // sameSite: none allows cookies to be sent in all contexts
+        sameSite: process.env.NODE_ENV === "production" ? 'none' : 'lax',
+    }
+}
+
+if (process.env.NODE_ENV === 'production') {
+    // must set for secure to work on HTTPs protocol
+    app.set('trust proxy', 1) // trust first proxy
+}
+
+app.use(session(sess))
+app.use(express.json({
+    limit: '50mb'
+}));
+
+HelloController(app);
+UsersController(app);
+AuthenticationController(app);
 app.listen(process.env.PORT || 4000);
---
+++ controllers/users/users-controller.js
@@ -0,0 +1,33 @@
+import * as userDao from "../../daos/users/users-dao.js";
+
+const UserController = (app) => {
+    app.get('/api/users/:uid/excluded', findAllUsersWithoutMe);
+    app.get('/api/users/:uid', findUserByUserId);
+    app.put('/api/users/:uid', updateUser);
+}
+
+const findAllUsersWithoutMe = async (req, res) => {
+    const userId = req.params.uid;
+    // filter out logged in user
+    const users = await userDao.findAllUsersWithoutMe(userId);
+    res.json(users);
+}
+
+const findUserByUserId = async (req, res) => {
+    try {
+        const userId = req.params.uid;
+        const user = await userDao.findUserById(userId)
+        res.json(user);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+}
+
+const updateUser = async (req, res) => {
+    const userId = req.params['uid'];
+    const updates = req.body;
+    await userDao.updateUser(userId, updates);
+    res.sendStatus(200);
+}
+
+export default UserController;
