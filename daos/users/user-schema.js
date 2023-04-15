0fb1d6de5f759784671f291dc4aa98481dae35a5
lnyrlyed <yang.ruini@northeastern.edu>
2023-04-14 22:26:20 -0400

    add paid and unpaid

--- daos/users/user-schema.js
+++ daos/users/user-schema.js
@@ -4,6 +4,7 @@ const UserSchema = mongoose.Schema({
     username: {type: String, required: true},
     password: {type: String, required: true},
     email: {type: String, required: true},
+    paidSubscriber: {type: String, required: true},
     planNumber: Number,
 }, {collection: 'test'})
 
