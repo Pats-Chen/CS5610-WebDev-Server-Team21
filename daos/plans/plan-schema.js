81dba7f4e29f67c8f4f9000eaefd1f0aacbab9ae
Pats-Chen <chenjiazhe@outlook.com>
2023-04-23 19:14:59 -0400

    Modify cors policy

--- daos/plans/plan-schema.js
+++ daos/plans/plan-schema.js
@@ -1,20 +1,20 @@
 import mongoose from "mongoose";
 
 const PlanSchema = mongoose.Schema({
-                                       // planId: {type: mongoose.Schema.Types.ObjectId, required: true},
-                                       userId: {type: mongoose.Schema.Types.ObjectId, required: true},
-                                       planName: {type: String, required: true},
-                                       planDescription: String,
-                                       planOwner: mongoose.Schema.Types.ObjectId,
-                                       locations: [{
-                                           address: String,
-                                           location: {
-                                               lat: Number,
-                                               lng: Number
-                                           },
-                                           name: String,
-                                           placeId: String,
-                                       }]
-                                   }, {collection: 'plans'})
+                                        // planId: {type: mongoose.Schema.Types.ObjectId, required: true},
+                                        userId: {type: mongoose.Schema.Types.ObjectId, required: true},
+                                        planName: {type: String, required: true},
+                                        planDescription: String,
+                                        planOwner: mongoose.Schema.Types.ObjectId,
+                                        locations: [{
+                                            address: String,
+                                            location: {
+                                                lat: Number,
+                                                lng: Number
+                                            },
+                                            name: String,
+                                            placeId: String,
+                                        }]
+                                    }, {collection: 'plans'})
 
 export default PlanSchema;
